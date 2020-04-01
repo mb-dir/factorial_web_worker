607f178d395f0a1425ed237e33c3ceeb14d71f88
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-01 20:02:40 +0200

    Create variables

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('#formData');
+const silniaInfoPlace = document.querySelector('#silniaInfo');
 
 formData.addEventListener('submit', (e)=>{
     e.preventDefault();
+    const number = parseInt(document.querySelector('#silnia').value);
 });
