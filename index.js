7c7c2592bc377ae699156a9d29ef51b80181dd9c
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-01 20:06:23 +0200

    Create function which return silnia

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ formData.addEventListener('submit', (e)=>{
     e.preventDefault();
     const number = parseInt(document.querySelector('#silnia').value);
 });
+
+function silnia(number){
+    if(number === 0){
+        return 0;
+    } else if (number === 1){
+        return 1;
+    } else if (number === 2){
+        return 2;
+    }else{
+        return silnia(number-1)*number;
+    }
+}
